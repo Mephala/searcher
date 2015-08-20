09401f89647154d43763de577146ce2c4a13d57c
gokhanabi <gokhanabi@gokhanabi-laptop>
2015-08-20 23:20:42 +0300

    Initial commit.

---
+++ searcher/src/main/java/searcher/parser/FileNameParser.java
@@ -0,0 +1,13 @@
+package searcher.parser;
+
+import java.io.File;
+
+public class FileNameParser {
+
+	private final File rootFolderToParse;
+
+	public FileNameParser(File rootFolderToParse) {
+		this.rootFolderToParse = rootFolderToParse;
+	}
+
+}
